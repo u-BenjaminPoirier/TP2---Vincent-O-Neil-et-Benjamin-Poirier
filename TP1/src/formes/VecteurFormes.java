ee57f894add06754750559a3b10cf9ef507a9367
Benjamin P <benjopoirier@gmail.com>
2023-02-27 13:48:17 -0500

    Tentative de Remplir le vecteur(en equipe)

--- TP1/src/formes/VecteurFormes.java
+++ TP1/src/formes/VecteurFormes.java
@@ -13,6 +13,44 @@ public class VecteurFormes implements IVecteurFormes{
 
     @Override
     public void remplir(int nbrElements) throws ArrayIndexOutOfBoundsException {
+        if (nbrElements <= 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        int i = 0;
+
+        do {
+            Forme t = new Triangle(1,1,1);
+            Vecteur.add(t);
+
+            i++;
+
+            Forme r = new Rectangle(1,1);
+            Vecteur.add(r);
+
+            i++;
+
+            Forme c = new Cercle(1);
+            Vecteur.add(c);
+
+            i++;
+        } while (i < nbrElements);
+
+        
+        i = 0;
+
+        while (i < nbrElements){
+            for (int j = 0; j < 3; j++) {
+                Vecteur.get(i).setCouleur();
+
+
+                i++;
+            }
+        }
+
+
+
+
 
     }
 

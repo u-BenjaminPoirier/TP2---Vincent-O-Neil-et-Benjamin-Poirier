107f71ef457b014826510efa3ee951d89482078f
Benjamin P <benjopoirier@gmail.com>
2023-02-27 12:33:02 -0500

    Ajout de la classe Vecteurs Formes

---
+++ TP1/src/formes/VecteurFormes.java
@@ -0,0 +1,25 @@
+package formes;
+
+import java.util.ArrayList;
+
+public class VecteurFormes implements IVecteurFormes{
+    @Override
+    public ArrayList<Forme> getVecteur() {
+        return null;
+    }
+
+    @Override
+    public void remplir(int nbrElements) throws ArrayIndexOutOfBoundsException {
+
+    }
+
+    @Override
+    public void trier() {
+
+    }
+
+    @Override
+    public void melanger() {
+
+    }
+}

49a0d65c34113c5f154a03c25267ff2a99e96907
Vincent O'Neil <2035536@cegeplimoilou.com>
2023-03-01 09:22:43 -0500

    Modification de ajouterEspaces et ajout de getNomForme

--- TP1/src/jeu/JeuMemoire.java
+++ TP1/src/jeu/JeuMemoire.java
@@ -25,15 +25,13 @@ public class JeuMemoire implements IJeuMemoire {
 
 
     private String ajouterEspaces(int nbEspaces, String forme) {
-        Forme r = new Rectangle(1,1);
-        int pColonne = 0;
-        for (int i = 0; i < nbEspaces; i++){
-            grilleDeJeu[pColonne][i] = r;
-            if (i + 1 % 6 == 0){
-                pColonne += 1;
-            }
+        int i = 0;
+        while (i < nbEspaces){
+            for(int j = 0; j < nbEspaces; j++){
+                forme += " ";
+                i++;
         }
-
+        return forme;
     }
 
     private Point choisirForme(){
@@ -56,7 +54,6 @@ public class JeuMemoire implements IJeuMemoire {
 
     @Override
     public boolean jouerHumain(int ligne, int colonne) {
-        return false;
     }
 //get
 
@@ -66,7 +63,7 @@ public class JeuMemoire implements IJeuMemoire {
 
     @Override
     public String getNomForme(int ligne, int colonne) {
-        return null;
+        return String.valueOf(grilleDeJeu[ligne][colonne]).strip();
     }
 
     @Override

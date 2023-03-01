241fae6823dfa2bbb773b2635d7151191d04d7e5
Benjamin <benjopoirier@gmail.com>
2023-03-01 12:51:13 +0100

    Ajout de: toString court, trier, melanger,to String, add forme, valider

--- TP1/src/formes/VecteurFormes.java
+++ TP1/src/formes/VecteurFormes.java
@@ -22,19 +22,11 @@ public class VecteurFormes implements IVecteurFormes {
     /**
      * Méthode qui nous permet de remplir le vecteur de forme selon le nombre de fois voulues
      * @param nbrElements le nombre de formes voulues
-     * @throws ArrayIndexOutOfBoundsException
      */
-    @Override
-<<<<<<< Updated upstream
-    public void remplir(int nbrElements) {
 
-        if (nbrElements <= 0) {
-=======
-    public void remplir(int nbrElements) throws ArrayIndexOutOfBoundsException {
-<<<<<<< Updated upstream
-        if (nbrElements <= 0){
->>>>>>> Stashed changes
-            throw new ArrayIndexOutOfBoundsException();
+    public void remplir(int nbrElements){
+        if(!(validerNbrFormes(nbrElements))){
+            throw new IndexOutOfBoundsException();
         }
 
         Couleur[] listeCouleur = {BLEU, JAUNE, NOIR, ORANGE, ROUGE, VERT,};
@@ -64,25 +56,16 @@ public class VecteurFormes implements IVecteurFormes {
                 }
             }
         }
-<<<<<<< Updated upstream
-=======
-
-
-
-
-=======
-        if(nbrElements <= 0){
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        for (int i = 0; i < nbrElements; i++){
->>>>>>> Stashed changes
-
-        }
->>>>>>> Stashed changes
     }
     @Override
     public void trier() {
-
+        for (int i = 0; i < Vecteur.size(); i++) {
+            for (int j = 0; j < Vecteur.size() ; j++) {
+                if (0 < Vecteur.get(j).compareTo(Vecteur.get(j + 1)){
+                    permuter(j,j+1);
+                }
+            }
+        }
     }
 
     @Override
@@ -102,19 +85,40 @@ public class VecteurFormes implements IVecteurFormes {
 
 
     private void addForme(Forme forme, Couleur c) {
-
+        forme.setCouleur(c);
+        Vecteur.add(forme);
     }
 
-    private void permuter(int forme1, int forme2) {
+    private void permuter(int index1, int index2) {
+        Forme o1;
+        Forme o2;
+
+         o1 = Vecteur.get(index1);
+         o2 = Vecteur.get(index2);
+
+        Vecteur.remove(index1);
+        Vecteur.add(index1, o2);
+        Vecteur.remove(index2);
+        Vecteur.add(index2, o1);
 
     }
 
     public String toString() {
+        StringBuilder str = new StringBuilder();
+        int i = 0;
 
+        while (i<Vecteur.size()){
+            str.append(Vecteur.get(i).toStringCourt()).append("\n");
+            i++;
+        }
+
+        return str.toString();
     }
 
     private boolean validerNbrFormes(int nbrForme) {
-
+        if (nbrForme <= 0){
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
 

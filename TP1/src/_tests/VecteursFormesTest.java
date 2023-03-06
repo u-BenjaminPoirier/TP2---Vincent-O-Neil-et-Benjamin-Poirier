98290b9371b9232575fac1b15c82f67ea567b4a1
Benjamin <benjopoirier@gmail.com>
2023-03-06 22:21:41 +0100

    tests Semi-complétés

--- TP1/src/_tests/VecteursFormesTest.java
+++ TP1/src/_tests/VecteursFormesTest.java
@@ -1,4 +1,41 @@
 package _tests;
 
+import formes.VecteurFormes;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class VecteursFormesTest {
+
+    @Test
+    void testerTrier() {
+        VecteurFormes v = new VecteurFormes();
+        VecteurFormes v1 = new VecteurFormes();
+
+
+        v.remplir(3);
+        v.melanger();
+        v.trier();
+        v1.remplir(3);
+        v1.melanger();
+        v1.trier();
+
+        assert(v.getVecteur().equals(v1.getVecteur()));
+    }
+
+    @Test
+    void testerGetVecteur() {
+        VecteurFormes v = new VecteurFormes();
+        VecteurFormes v2 = new VecteurFormes();
+
+        v.remplir(14);
+        v2.remplir(14);
+
+        assert(v.getVecteur().equals(v2.getVecteur()));
+    }
+
+    @Test
+    void testerRemplir() {
+        VecteurFormes v = new VecteurFormes();
+
+    }
 }

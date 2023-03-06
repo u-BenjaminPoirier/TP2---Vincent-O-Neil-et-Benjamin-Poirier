6945455184fcad28a5e05be7f4b3f26725dee1c4
Benjamin <benjopoirier@gmail.com>
2023-03-06 20:06:31 +0100

    Ajout de la javadoc de VecteurFormes

--- TP1/src/formes/VecteurFormes.java
+++ TP1/src/formes/VecteurFormes.java
@@ -12,12 +12,14 @@ public class VecteurFormes implements IVecteurFormes {
 
     /**
      * Obtenir le vecteur de formes
+     *
      * @return le vecteur de formes
      */
     @Override
     public ArrayList<Forme> getVecteur() {
         return Vecteur;
     }
+
     /**
      * Mettre le nombre de formes voulues dans le vecteur. Les formes qui seront
      * placées dans le vecteur devront être générées de la façon suivante:
@@ -67,6 +69,7 @@ public class VecteurFormes implements IVecteurFormes {
             }
         }
     }
+
     /**
      * Trier en ordre croissant le vecteur de formes en utilisant le "compareTo"
      * disponible sur les objets.
@@ -77,10 +80,11 @@ public class VecteurFormes implements IVecteurFormes {
             for (int j = 0; j < Vecteur.size(); j++) {
                 if (0 < Vecteur.get(j).compareTo(Vecteur.get(j + 1))) {
                     permuter(j, j + 1);
-            }
                 }
+            }
         }
     }
+
     /**
      * Mélanger les éléments du vecteur.
      */
@@ -99,11 +103,24 @@ public class VecteurFormes implements IVecteurFormes {
         }
     }
 
+    /**
+     * Ajoute une forme au vecteur
+     *
+     * @param forme forme a ajouter
+     * @param c     Est pour couleur, sera la couleur de la forme
+     */
 
     private void addForme(Forme forme, Couleur c) {
         forme.setCouleur(c);
         Vecteur.add(forme);
     }
+
+    /**
+     * Permet de permuter deux elements du vecteur
+     *
+     * @param index1 index de l'element 1 a echanger
+     * @param index2 index de l'element 2 a echanger
+     */
 
     private void permuter(int index1, int index2) {
         Forme o1;
@@ -119,18 +136,28 @@ public class VecteurFormes implements IVecteurFormes {
 
     }
 
+    /**
+     * Transforme la classe en String
+     *
+     * @return une String du vecteur
+     */
+
     public String toString() {
         StringBuilder str = new StringBuilder();
         int i = 0;
 
         while (i < Vecteur.size()) {
             str.append(Vecteur.get(i).toStringCourt()).append("\n");
-        }
             i++;
-
+        }
         return str.toString();
     }
 
+    /**
+     * Permet de valider le nombre de forme (est invalide lorsque negatif)
+     * @param nbrForme nombre de forme a ajouter dans le vecteur
+     * @return si la forme est valide ou non
+     */
     private boolean validerNbrFormes(int nbrForme) {
         return nbrForme > 0;
     }
